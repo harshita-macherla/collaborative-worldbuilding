dab7084cfa27855517cca55105f7a36cd5ee39b1
harshita <harshita27.m@gmail.com>
2026-07-01 15:47:52 +0530

    fix mobile cookie issue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,7 +6,7 @@ const sendTokenResponse=(user,statusCode, res)=>{
     res.cookie("token", token, {
       httpOnly: true,
       secure: true, // always true in production
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(statusCode).json({
--- server/index.js
+++ server/index.js
@@ -19,8 +19,8 @@ app.use(cors({
     ? process.env.CLIENT_URL
     : "http://localhost:5173",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
 }));
 app.set("trust proxy", 1);
 
